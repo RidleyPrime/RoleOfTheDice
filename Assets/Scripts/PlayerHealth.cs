31c939f9654970b999e0a65778bfbf08182f7942
Acasez <edvin.sanne@gmail.com>
2022-07-16 21:45:16 +0200

    Healthbar and restart

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,47 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health; //the number that is our actual health
     public int maxHealth = 100; //our maximum health
-   // public Slider healthSlider; //the slider that represents our health
+    public Slider healthSlider; //the slider that represents our health
 
-   // void Start()
-   // {
-     //   health = maxHealth;
-       // healthSlider.maxValue = maxHealth;
-       // healthSlider.minValue = 0;
-       // healthSlider.SetDirection(Slider.Direction.LeftToRight, true); //I have no idea if this should be true or false
-   // }
+     public void Start()
+     {
+        health = maxHealth;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = health;
+     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //UI Mumbo Jumbo, checks health in real time. need to make sure this works
-    }
     public void heal(int healAmount)
     {
-        float tempHealth = health;
-        if(tempHealth + healAmount > maxHealth)
+        if(health + healAmount > maxHealth)
         {
             health = maxHealth;
+        }
+        else if (health + healAmount <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            //Die
         }
         else
         {
             health += healAmount;
         }
+        healthSlider.value = health;
     }
-    //make a damage function? or is this fine if we just pass the right colliders
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Enemy")
         {
-            if (other.gameObject.tag == "Enemy")
-            {
-                Debug.Log("Damage");
-                health--;
-              //  healthSlider.value = health;
+            Debug.Log("Damage");
+            heal (-1);
+            //  healthSlider.value = health;
         }
-        }
+    }
 }
